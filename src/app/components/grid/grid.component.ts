2ff11c86f323b60e7b685133326da41d84a2b56c
Pablo Depiné <pablodepine96@gmail.com>
2024-03-11 19:35:00 -0300

    ajusta componente Grid

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { FuncionariosService } from '../../services/funcionarios.service';
-import { Subscription, map } from 'rxjs';
+import { Subscription, elementAt, map } from 'rxjs';
 import { Funcionario } from '../../models/funcionario';
 import { CommonModule } from '@angular/common';
+import { ModalComponent } from '../modal/modal.component';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ModalComponent],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.scss',
   providers: [FuncionariosService],
 })
 export class GridComponent implements OnInit {
-  funcionarios: Funcionario[] = [];
   private subscription: Subscription = new Subscription();
+
+  showModal: boolean = false;
+  modalMode: string = '';
+  funcionario!: Funcionario;
+  funcionarioParaEditar!: Funcionario;
+  funcionarios: Funcionario[] = [];
+  toastMessage: string = '';
 
   constructor(private funcionariosService: FuncionariosService) {}
 
@@ -37,12 +46,15 @@ export class GridComponent implements OnInit {
     this.subscription.unsubscribe();
   }
 
-  editarFuncionario(idFuncionario: number) {
-    this.funcionarios.forEach((element) => {
-      if (element.id === idFuncionario) {
-        console.log(element);
-      }
-    });
+  adicionarFuncionario() {
+    this.modalMode = 'ADICIONAR';
+    this.showModal = true;
+  }
+
+  editarFuncionario(dadosFuncionario: Funcionario) {
+    this.modalMode = 'EDITAR';
+    this.funcionarioParaEditar = dadosFuncionario;
+    if (this.funcionarioParaEditar) this.showModal = true;
   }
 
   excluirFuncionario(idFuncionario: number) {
@@ -51,7 +63,32 @@ export class GridComponent implements OnInit {
         this.funcionarios.splice(index, 1);
       }
     });
+  }
 
-    console.log(this.funcionarios);
+  recebeDadosFuncionario(dadosFuncionario: Funcionario) {
+    this.modalMode !== 'EDITAR'
+      ? this.adicionaFuncionario(dadosFuncionario)
+      : this.editaFuncionario(dadosFuncionario);
+  }
+
+  adicionaFuncionario(dadosFuncionario: Funcionario) {
+    this.funcionarios.push(dadosFuncionario);
+    this.mostrarToast();
+  }
+
+  editaFuncionario(dadosFuncionario: Funcionario) {
+    this.funcionarios.forEach((element, index) => {
+      if (element.id === dadosFuncionario.id) {
+        this.funcionarios[index] = dadosFuncionario;
+      }
+    });
+    this.toastMessage = 'Usuário adicionado com sucesso';
+    this.mostrarToast();
+  }
+
+  mostrarToast() {
+    const toastEl = document.getElementById('meuToast');
+    const toast = new bootstrap.Toast(toastEl);
+    toast.show();
   }
 }
