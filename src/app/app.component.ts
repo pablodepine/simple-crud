c3e8cf646b6e5e7695b728fc05372a775cece798
Pablo Depiné <pablodepine96@gmail.com>
2024-03-11 19:34:37 -0300

    ajusta ordem de importação componente app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GridComponent, HttpClientModule],
+  imports: [RouterOutlet, HttpClientModule, GridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
